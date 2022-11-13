ff01f1600e4c6efcb5c1355f7d4d61a970ff2a3d
Manahil40 <bitf19a040@pucit.edu.pk>
2022-11-13 21:55:04 +0500

    Functionality commit

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -12,10 +13,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnEven, btnOdd;
-    TextView txtQ, txtans;
-    int numbers = 0;
-    int n = 0;
+    Button btnEven, btnOdd, btnNext;
+    TextView txtQ, txtQNo, txtscore, txtAns;
+    int numbers = 0, n = 1, score = 0;
+    String ans = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +24,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         txtQ = findViewById(R.id.txtQuestions);
+        txtQNo = findViewById(R.id.textQNo);
+        txtscore = findViewById(R.id.textScore);
+        txtAns = findViewById(R.id.textScore);
+
         btnEven = findViewById(R.id.even);
         btnEven.setOnClickListener(this);
+
         btnOdd = findViewById(R.id.odd);
         btnOdd.setOnClickListener(this);
+
+        btnNext = findViewById(R.id.Nextbtn);
+        btnNext.setOnClickListener(this);
 
         GenerateNumbers();
     }
@@ -36,20 +45,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Random rand = new Random();
 
         if (n <=5) {
+            n++;
             numbers = rand.nextInt(100) + 1;
-            txtQ.setText("Question "+n);
-            txtQ.setText(numbers + " Is Even or Odd?");
+            txtQNo.setText("Question # "+n);
+            txtQ.setText(numbers + " is Even or Odd?");
 
             if (numbers % 2 == 0) {
-
+                ans = "Even";
             } else {
-
+                ans = "Odd";
             }
         }
     }
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.even:
+                if (ans == "Even") {
+                    txtAns.setText("Correct Answer!");
+                    /*textViewAnswer.setBackgroundColor(getResources().getColor(R.color.green));*/
+                    score++;
+                    txtscore.setText("Score: " + score);
+                } else {
+                    txtAns.setText("Incorrect Answer!");
+                }
+                break;
 
+            case R.id.odd:
+                if (ans == "Odd") {
+                    txtAns.setText("Correct Answer!");
+                    /*textViewAnswer.setBackgroundColor(getResources().getColor(R.color.green));*/
+                    score++;
+                    txtscore.setText("Score: " + score);
+                } else {
+                    txtAns.setText("Incorrect Answer!");
+                }
+                break;
+        }
+        GenerateNumbers();
     }
 
 }
