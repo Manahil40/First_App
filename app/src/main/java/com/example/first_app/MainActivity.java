ca2b4803ab3a9c23f029826927e7f1a08dcd04da
Manahil40 <bitf19a040@pucit.edu.pk>
2022-11-13 20:18:24 +0500

    changes saved

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -7,19 +7,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btnEven, btnOdd;
     TextView txtQ, txtans;
+    int numbers = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtQ = findViewById(R.id.txtQuestions);
+
     }
 
     private void GenerateNumbers(){
+
+        Random rand = new Random();
+        numbers = rand.nextInt();
+
+        if(numbers % 2 == 0){
+
+        }
+
+        else{
+
+        }
 
     }
     @Override
