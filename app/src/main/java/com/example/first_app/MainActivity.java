4a0236d8a400b25053c1a38b637b8b88b19cbef7
Manahil40 <bitf19a040@pucit.edu.pk>
2022-11-13 19:07:45 +0500

    UI hanges saved

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.first_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnEven, btnOdd;
+    TextView txtQ, txtans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void GenerateNumbers(){
+
+    }
+    @Override
+    public void onClick(View view) {
+
+    }
+
 }
